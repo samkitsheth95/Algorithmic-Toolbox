d67cdd35cad7dda72a4c0a16b07dfce5ca82ee09
Samkit Sheth <samkitsheth95@gmail.com>
2020-08-28 21:33:21 -0700

    useless files deleted

--- week4_divide_and_conquer/3_improving_quicksort/morgan.py
+++
@@ -1,9 +0,0 @@
-text = "hello world"
-ans = ""
-for i in range(len(text)):
-    if text[i-1] == " " and i!= len(text)-1:
-        ans += text[i].capitalize()
-    else:
-        ans += text[i]
-
-print(ans)
